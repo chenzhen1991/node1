e04274e3410c3d42aeeb06656da157ef0a8f5386
Administrator <CZW0919@163.com>
2020-05-23 22:47:06 +0800

    sql创建表

--- 04/mysql/mysql2.js
+++ 04/mysql/mysql2.js
@@ -3,5 +3,27 @@
     const cfg ={
         host: 'localhost',
         user: 'root',
+        password:'130609zzr',
+        database: 'test'
     }
+
+    //创建连接
+    const connection = await mysql.createConnection(cfg)
+
+    // 查询
+    // 创建表
+    const CREATE_SQL = `CREATE TABLE IF NOT EXISTS test (
+        id INT NOT NULL AUTO_INCREMENT,
+        message VARCHAR(45) NULL,
+        PRIMARY KEY (id))`;
+    const INSERT_SQL = `INSERT INTO test(message) VALUES(?)`;
+    const SELECT_SQL = `SELECT * FROM test`;
+
+    // query
+    let ret = await connection.execute(CREATE_SQL);
+    console.log('create', ret);
+    ret = await connection.execute(INSERT_SQL, ['abc'])
+    console.log('insert:', ret)
+    ret = await connection.execute(SELECT_SQL)
+    console.log('insert:', ret)
 })()
